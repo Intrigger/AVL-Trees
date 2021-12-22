2e0e35f64dd1b8338ec3c515c5830f18e082a445
Intrigger <xintrigger@gmail.com>
2021-12-23 00:10:17 +0300

    добавлен класс и создана функция insert (только начало)

--- main.cpp
+++ main.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
 
+class AVLTree{
+
+private:
+    struct AVLNode{
+        int key;
+        AVLNode* left;
+        AVLNode* right;
+        AVLNode* parent;
+        int balance;
+    };
+
+    AVLNode* root;
+
+public:
+    AVLTree(){
+        root = nullptr;
+    }
+
+    void insert(int value){
+        return;
+    }
+
+};
+
+
+
+
 int main(){
 
     using std::cout;
 
     cout << "AVL Trees lab #4\n";
-    
+
+
 
     return 0;
 }
